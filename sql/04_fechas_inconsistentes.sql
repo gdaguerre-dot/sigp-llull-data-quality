5191590a8b59c0e1c4e65796e39ca69322231c06
Geronimo Daguerre <gerodaguerre@gmail.com>
2026-06-24 16:31:12 -0300

    Update 04_fechas_inconsistentes.sql

--- sql/04_fechas_inconsistentes.sql
+++ sql/04_fechas_inconsistentes.sql
@@ -1,1 +1,14 @@
+/*
+Objetivo:
+Detectar inconsistencias temporales.
+Dimensión de calidad:
+Consistencia Temporal
+*/
 
+SELECT
+    x_empleado,
+    f_tompos,
+    f_cese
+FROM lloc_treball_empleats
+WHERE f_cese IS NOT NULL
+AND f_cese < f_tompos;
