650e65c47d961f342e39d37fff3e915d24db3c78
Geronimo Daguerre <gerodaguerre@gmail.com>
2026-06-24 16:29:20 -0300

    Update 03_cargos_invalidos.sql

--- sql/03_cargos_invalidos.sql
+++ sql/03_cargos_invalidos.sql
@@ -1,1 +1,14 @@
+/*
+Objetivo:
+Detectar cargos inexistentes en catálogo.
+Dimensión de calidad:
+Validez
+*/
 
+SELECT
+    e.x_empleado,
+    e.c_cuerpo AS cargo
+FROM empleados e
+LEFT JOIN cargos c
+    ON e.c_cuerpo = c.ccargo
+WHERE c.ccargo IS NULL;
