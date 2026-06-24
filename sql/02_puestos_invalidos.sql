743e0d4ea38bd0d667969c7020f0c3a358ca602f
Geronimo Daguerre <gerodaguerre@gmail.com>
2026-06-24 16:28:41 -0300

    Update 02_puestos_invalidos.sql

--- sql/02_puestos_invalidos.sql
+++ sql/02_puestos_invalidos.sql
@@ -1,1 +1,14 @@
+/*
+Objetivo:
+Detectar puestos inexistentes en catálogo.
+Dimensión de calidad:
+Integridad Referencial
+*/
 
+SELECT
+    e.x_empleado,
+    e.x_puestod
+FROM empleados e
+LEFT JOIN puestos p
+    ON e.x_puestod = p.x_puesto
+WHERE p.x_puesto IS NULL;
