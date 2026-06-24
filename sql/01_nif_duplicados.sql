fc8abd0e4b04a4eeda60e4beb43971806d3c7754
Geronimo Daguerre <gerodaguerre@gmail.com>
2026-06-24 16:28:08 -0300

    Update 01_nif_duplicados.sql

--- sql/01_nif_duplicados.sql
+++ sql/01_nif_duplicados.sql
@@ -1,1 +1,13 @@
+/*
+Objetivo:
+Detectar empleados con documento identificativo duplicado.
+Dimensión de calidad:
+Unicidad
+*/
 
+SELECT
+    c_numide,
+    COUNT(*) AS cantidad_registros
+FROM empleados
+GROUP BY c_numide
+HAVING COUNT(*) > 1;
