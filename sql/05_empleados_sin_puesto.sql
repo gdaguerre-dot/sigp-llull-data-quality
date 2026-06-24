6c6b820fb0147030b5e2f8cfb5b129c23aa40bc4
Geronimo Daguerre <gerodaguerre@gmail.com>
2026-06-24 16:31:43 -0300

    Update 05_empleados_sin_puesto.sql

--- sql/05_empleados_sin_puesto.sql
+++ sql/05_empleados_sin_puesto.sql
@@ -1,1 +1,15 @@
+/*
+Objetivo:
+Detectar empleados activos sin puesto vigente.
+Dimensión de calidad:
+Completitud
+*/
 
+SELECT
+    e.x_empleado,
+    e.t_nombre,
+    e.t_apellido1
+FROM empleados e
+LEFT JOIN lloc_treball_empleats l
+    ON e.x_empleado = l.x_empleado
+WHERE l.x_puesto IS NULL;
